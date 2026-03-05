972c0d8971680a4c02c99ca6afaa37d64de819b7
KingScholarship <chanseytheopiest@gmail.com>
2026-03-05 23:50:53 +0800

    Added:  - Key Handle (accepts: WASD)  - Game Panel  - Game Loop (FPS: 60)  - Player (unfinished)

--- src/engine/GamePanel.java
+++ src/engine/GamePanel.java
@@ -1,4 +1,41 @@
 package engine;
 
-public class GamePanel {
+import input.KeyboardHandler;
+import overworld.Player;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+
+    //Screen Settings
+    public final int ORIGINAL_TILE_SIZE = 32;
+    final int SCALE = 3;
+
+    final int TILE_SIZE = ORIGINAL_TILE_SIZE * SCALE;
+    final int MAX_SCREEN_COL = 32;
+    final int MAX_SCREEN_ROW = 28;
+    final int SCREEN_WIDTH = TILE_SIZE * MAX_SCREEN_COL;
+    final int SCREEN_HEIGHT = TILE_SIZE * MAX_SCREEN_ROW;
+
+    final int FPS = 60;
+
+    public KeyboardHandler input = new KeyboardHandler();
+    Player player = new Player(this, input);
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.BLACK);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(input);
+        this.setFocusable(true);
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D graphics2D = (Graphics2D) g;
+        player.draw(graphics2D);
+        graphics2D.dispose();
+    }
+
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,23 @@
 package main;
 
+import javax.swing.*;
+import engine.*;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("test");
-        System.out.println("test");
+        JFrame windows = new JFrame();
+        windows.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        windows.setResizable(false);
+        windows.setTitle("Rot");
+
+        GamePanel gamePanel = new GamePanel();
+        windows.add(gamePanel);
+
+        windows.pack();
+
+        windows.setLocationRelativeTo(null);
+        windows.setVisible(true);
+
+        new GameLoop(gamePanel).startGameThread(gamePanel);
     }
 }
